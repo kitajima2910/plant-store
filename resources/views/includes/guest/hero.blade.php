f09395c7aaa74d426db93c67f771361022bd00c7
Huy <toilahuy098@gmail.com>
2020-07-29 15:30:35 +0700

    show slider show product saleoff hide arrow input

--- resources/views/includes/guest/hero.blade.php
+++ resources/views/includes/guest/hero.blade.php
@@ -1,49 +1,25 @@
 <section class="hero-area">
     <div class="hero-post-slides owl-carousel">
+        @foreach ($sliders as $slider)
 
         <!-- Single Hero Post -->
         <div class="single-hero-post bg-overlay">
             <!-- Post Image -->
-            <div class="slide-img bg-img" style="background-image: url({{ asset('public/frontend/img/bg-img/1.jpg') }});"></div>
+            <div class="slide-img bg-img" style="background-image: url({{ asset($slider->feature_image_path) }});"></div>
             <div class="container h-100">
                 <div class="row h-100 align-items-center">
                     <div class="col-12">
                         <!-- Post Content -->
                         <div class="hero-slides-content text-center">
-                            <h2>Plants exist in the weather and light rays that surround them</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque ante
-                                nec ipsum iaculis, ac iaculis ipsum porttitor. Vivamus cursus nisl lectus, id mattis
-                                nisl lobortis eu. Duis diam augue, dapibus ut dolor at, mattis maximus dolor.</p>
+                            {{!! $slider->content !!}}
                             <div class="welcome-btn-group">
-                                <a href="#" class="btn alazea-btn mr-30">CONTACT US</a>
+                                <a href="{{$slider->link}}" class="btn alazea-btn mr-30">CONTACT US</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-        <!-- Single Hero Post -->
-        <div class="single-hero-post bg-overlay">
-            <!-- Post Image -->
-            <div class="slide-img bg-img" style="background-image: url({{ asset('public/frontend/img/bg-img/2.jpg') }});"></div>
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <!-- Post Content -->
-                        <div class="hero-slides-content text-center">
-                            <h2>Plants exist in the weather and light rays that surround them</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque ante
-                                nec ipsum iaculis, ac iaculis ipsum porttitor. Vivamus cursus nisl lectus, id mattis
-                                nisl lobortis eu. Duis diam augue, dapibus ut dolor at, mattis maximus dolor.</p>
-                            <div class="welcome-btn-group">
-                                <a href="#" class="btn alazea-btn mr-30">CONTACT US</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        </div>   
+        @endforeach
     </div>
 </section>
